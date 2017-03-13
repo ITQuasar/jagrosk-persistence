5c086f171596bee7c24c3a457a16aa9fdd4966eb
Guilherme I F L Weizenmann <guilherme@itquasar.com>
2017-03-12 23:22:31 -0300

    Fix OneShotRepository returns

--- src/main/java/org/itquasar/multiverse/jagrosk/persistence/OneShotRepository.java
+++ src/main/java/org/itquasar/multiverse/jagrosk/persistence/OneShotRepository.java
@@ -1,5 +1,6 @@
 package org.itquasar.multiverse.jagrosk.persistence;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -7,7 +8,7 @@ import java.util.function.Supplier;
 /**
  * Created by guilherme on 29/01/17.
  */
-public class OneShotRepository<I, E extends JagroskEntity<I>> implements Repository<I, E>{
+public class OneShotRepository<I, E extends JagroskEntity<I>> implements Repository<I, E> {
 
     protected final Repository<I, E> repository;
 
@@ -15,7 +16,17 @@ public class OneShotRepository<I, E extends JagroskEntity<I>> implements Reposit
         this.repository = repository;
     }
 
-    protected <T> T performAndReturn(Supplier<T> action){
+    protected <T> Optional<T> performAndReturnOptional(Supplier<Optional<T>> action) {
+        Optional<T> opt = performAndReturn(action);
+        return opt == null ? Optional.empty() : opt;
+    }
+
+    protected <T> List<T> performAndReturnList(Supplier<List<T>> action) {
+        List<T> lst = performAndReturn(action);
+        return lst == null ? Collections.emptyList() : lst;
+    }
+
+    protected <T> T performAndReturn(Supplier<T> action) {
         T t = action.get();
         this.close();
         return t;
@@ -23,42 +34,42 @@ public class OneShotRepository<I, E extends JagroskEntity<I>> implements Reposit
 
     @Override
     public Optional<E> add(E entity) {
-        return performAndReturn(() -> this.repository.add(entity));
+        return performAndReturnOptional(() -> this.repository.add(entity));
     }
 
     @Override
     public Optional<E> remove(E entity) {
-        return performAndReturn(()->this.repository.remove(entity));
+        return performAndReturnOptional(() -> this.repository.remove(entity));
     }
 
     @Override
     public Optional<E> remove(I id) {
-        return performAndReturn(()->this.repository.remove(id));
+        return performAndReturnOptional(() -> this.repository.remove(id));
     }
 
     @Override
     public Optional<E> update(E entity) {
-        return performAndReturn(() -> this.repository.update(entity));
+        return performAndReturnOptional(() -> this.repository.update(entity));
     }
 
     @Override
     public List<E> listAll() {
-        return performAndReturn(() -> this.repository.listAll());
+        return performAndReturnList(() -> this.repository.listAll());
     }
 
     @Override
     public Optional<E> findById(I id) {
-        return performAndReturn(() -> this.repository.findById(id));
+        return performAndReturnOptional(() -> this.repository.findById(id));
     }
 
     @Override
     public List<E> findBy(String propertyName, Object value) {
-        return performAndReturn(() -> this.repository.findBy(propertyName, value));
+        return performAndReturnList(() -> this.repository.findBy(propertyName, value));
     }
 
     @Override
     public List<E> findBy(JagroskPredicate predicate) {
-        return performAndReturn(() -> this.repository.findBy(predicate));
+        return performAndReturnList(() -> this.repository.findBy(predicate));
     }
 
     @Override
